8b01bbb98a23d4aa02fbfbe68d8706182fef99c1
Slimane Rhaytou <rhaytouslimane@gmail.com>
2023-11-30 09:36:29 -0800

    Commit message

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
 
 if __name__ == "__main__":
     import sys
-    add_arg(sys.argv)
+
+    total = 0
+    length = len(sys.argv) - 1
+    for x in range(length):
+        total += int(sys.argv[x + 1])
+    print("{}".format(total))
