8818e70cf613f222986c3f75ad84a79fc9f43023
Slimane Rhaytou <rhaytouslimane@gmail.com>
2023-11-30 09:35:05 -0800

    Commit message

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+
 
 if __name__ == "__main__":
     import sys
-    print_arg(sys.argv)
+    length = len(sys.argv) - 1
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, sys.argv[1]))
+    else:
+        print("{} arguments:".format(length))
+        for i in range(length):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
